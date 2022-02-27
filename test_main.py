d1bc2ec3d3289327648338289052b3ac949d70b7
Fangting Ma <fm128@duke.edu>
2022-02-27 00:45:08 +0000

    test_cal_profit

--- test_main.py
+++ test_main.py
@@ -4,4 +4,4 @@ def test_add():
     assert add(3, 1) == {"total":4}
     
 def test_cal_profit():
-    assert cal_profit(100, 120) == {"profit_margin":0.2}
+    assert cal_profit(80, 100) == {"profit_margin":0.2}
